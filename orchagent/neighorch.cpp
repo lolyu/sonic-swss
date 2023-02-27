edda8a0c75ad77a75d5e122696bafed7a49ec9f0
Longxiang Lyu <lolv@microsoft.com>
2023-02-27 14:21:26 +0000

    Ignore zero-mac events for enabled neighbors
    Signed-off-by: Longxiang Lyu <lolv@microsoft.com>

--- orchagent/neighorch.cpp
+++ orchagent/neighorch.cpp
@@ -739,20 +739,32 @@ void NeighOrch::doTask(Consumer &consumer)
                     mac_address = MacAddress(fvValue(*i));
             }
 
-            if (m_syncdNeighbors.find(neighbor_entry) == m_syncdNeighbors.end()
-                    || m_syncdNeighbors[neighbor_entry].mac != mac_address)
+            auto nbr_not_found = (m_syncdNeighbors.find(neighbor_entry) == m_syncdNeighbors.end());
+            if (nbr_not_found || m_syncdNeighbors[neighbor_entry].mac != mac_address)
             {
-                // only for unresolvable neighbors that are new
                 if (!mac_address)
                 {
-                    if (addZeroMacTunnelRoute(neighbor_entry, mac_address))
+                    if (nbr_not_found)
                     {
-                        it = consumer.m_toSync.erase(it);
+                        // only for unresolvable neighbors that are new
+                        if (addZeroMacTunnelRoute(neighbor_entry, mac_address))
+                        {
+                            it = consumer.m_toSync.erase(it);
+                        }
+                        else
+                        {
+                            it++;
+                            continue;
+                        }
                     }
                     else
                     {
-                        it++;
-                        continue;
+                        /*
+                         * FIXME: Handle the scenario if zero mac neighbor event
+                         * is received, but the neighbor is resolved already.
+                         * Ignore the zero mac neighbor events in this case for now.
+                         */
+                        it = consumer.m_toSync.erase(it);
                     }
                 }
                 else if (addNeighbor(neighbor_entry, mac_address))
